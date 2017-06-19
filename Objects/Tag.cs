a285fba311a27f5362a20a62df3ae69e80c5d616
John Dowd and Jordan Loop <john+jordan@>
2017-06-19 12:49:06 -0700

    add GetAll_DatabaseIsEmpty_True() to TagTest.cs and SnippetTest.cs; add GetAll() to Tag.cs and Snippet.cs, update README.md; all tests are passing

--- Objects/Tag.cs
+++ Objects/Tag.cs
@@ -6,7 +6,47 @@ namespace SnippetTool
 {
   public class Tag
   {
+    public string Text {get; set; }
+    public int Id {get; set; }
 
+    public Tag(string newText, int newId = 0 )
+    {
+      Text = newText;
+      Id = newId;
+    }
+
+
+
+    //GetAll()
+    public static List<Tag> GetAll()
+    {
+      List<Tag> allTags = new List<Tag>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM tag;", conn );
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int tagId = rdr.GetInt32(0 );
+        string tagName = rdr.GetString(1 );
+
+        Tag newTag = new Tag(tagName, tagId );
+        allTags.Add(newTag );
+      }
+
+      if (rdr != null )
+      {
+        rdr.Close();
+      }
+      if (conn != null )
+      {
+        conn.Close();
+      }
+      return allTags;
+    }
 
 
   }
