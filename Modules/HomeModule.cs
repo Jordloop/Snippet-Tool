3c80445af94a385a91754c33b387baf713e7fa08
Andrew Glines <aglines-epicodus@gmail.com>
2017-06-19 13:52:26 -0700

    update homemodule with routing for some Get and Post methods ; update all views with shared html layout

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,12 +13,23 @@ namespace SnippetTool
         return View["tag_view.cshtml", allTags];
       };
 
+      Get["/snippet_view"] = _ => {
+        List<Snippet> allSnippets = Snippet.GetAll;
+        return View["snippet_view", allSnippets];
+      };
 
+      Post["/tag_new"] = _ = {
+        Tag newTag = new Tag(Request.Form["tag-name"]);
+        newTag.Save();
+        List<Tag> allTags = Tag.GetAll();
+        return View["tag_view.cshtml", allTags];
+      };
 
-
-
-
-
+      Post["/snippet_new"] = _ = {
+        Snippet newSnippet = new Snippet(Request.Form["snippet-name"]);
+        List<Snippet> allSnippets = Snippet.GetAll();
+        return View["snippet_view", allSnippets];
+      };
 
 
 
